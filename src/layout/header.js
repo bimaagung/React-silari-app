02a64c0f6235d613209251e1ee39f3c525f08276
bimaotet <otetnews@gmail.com>
2018-07-11 01:47:28 +0700

    layout search, grid card

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,26 +1,70 @@
 import React, { Component } from 'react'
-import { Input, Menu, Button, Header } from 'semantic-ui-react'
+import { Input, Menu, Button, Header, Segment, Form } from 'semantic-ui-react'
+
+
+const divSearch = {
+    width: 500
+}
+
+const centerSearch = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
 
 export default class HeaderNav extends Component {
     render() {
         return (
-            <Menu stackable>
-                <Menu.Menu position='left'>
-                    <Menu.Item>
-                    <h3>Silari [ Sistem Alumni Riptek ]</h3>
-                    </Menu.Item>
-                </Menu.Menu>
-                <Menu.Menu position='center'>
-                    <Menu.Item>
-                        <Input icon='search' size='large' placeholder='Search...' />
-                    </Menu.Item>
-                </Menu.Menu>
-                <Menu.Menu position='right'>
-                    <Menu.Item>
-                        <Button primary>Sign up</Button>
-                    </Menu.Item>
-                </Menu.Menu>
-            </Menu>
+            <div>
+                <Menu stackable>
+                    <Menu.Menu position='left'>
+                        <Menu.Item>
+                            <h3>Silari [ Sistem Alumni Riptek ]</h3>
+                        </Menu.Item>
+                    </Menu.Menu>
+                    <Menu.Menu position='center'>
+                        <Menu.Item>
+                            <Input icon='search' size='large' placeholder='Search...' />
+                        </Menu.Item>
+                    </Menu.Menu>
+                    <Menu.Menu position='right'>
+                        <Menu.Item>
+                            <Button color='blue'>Fungsionaris</Button>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Button color='olive'>PTI</Button>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Button color='red'>Robotik</Button>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Button primary>Sign up</Button>
+                        </Menu.Item>
+                    </Menu.Menu>
+                </Menu>
+                <div style={centerSearch}>
+                <Segment style={divSearch}>
+                    <Form>
+                        <Form.Group>
+                            <Form.Input label='Nama' placeholder='First Name' width={12} />
+                            <Form.Input label='Bidang' placeholder='Middle Name' width={12} />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Input label='Tahun Masuk' placeholder='First Name' width={12} />
+                            <Form.Input label='Asal' placeholder='Middle Name' width={12} />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Checkbox label='Fungsionaris' width={4} />
+                            <Form.Checkbox label='Anggota Robotik' width={5} />
+                            <Form.Checkbox label='Anggota PTI' width={4} />
+                        </Form.Group>
+
+                        {/* <Button type='submit'>Submit</Button> */}
+                    </Form>
+                </Segment>
+                </div>
+            </div>
+
         )
     }
 }
--- src/main/mainPage.js
+++ src/main/mainPage.js
@@ -1,8 +1,111 @@
 import React from "react";
+import { Card, Icon, Segment, Label } from 'semantic-ui-react'
 
+const mainPage = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 15
+}
+
+const mainContent = {
+    width: 1137
+}
+
+const extra = (
+    <a>
+      <Icon name='user' />
+      16 Friends
+    </a>
+  )
 
 export default () =>
+<div style={mainPage}>
+    <div style={mainContent}>
+    <Segment padded>
+    <Label size='large' color='red' attached='top left'>Pengurus Harian</Label>
+        <Card.Group itemsPerRow={6}>
+             <Card
+                image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+                header='Elliot Baker'
+                //description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra={extra}
+            />
+             <Card
+                image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+                header='Elliot Baker'
+                //description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra={extra}
+            />
+             <Card
+                image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+                header='Elliot Baker'
+                //description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra={extra}
+            />
+             <Card
+                image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+                header='Elliot Baker'
+                //description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra={extra}
+            />
+             <Card
+                image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+                header='Elliot Baker'
+                //description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra={extra}
+            />
+             <Card
+                image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+                header='Elliot Baker'
+                //description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra={extra}
+            />
+        </Card.Group>
+        </Segment>
 
-   <h1> Center Pane </h1>
+        <Segment padded>
+    <Label size='large' color='blue' attached='top left'>Technopreneur</Label>
+        <Card.Group itemsPerRow={6}>
+             <Card
+                image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+                header='Elliot Baker'
+                //description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra={extra}
+            />
+             <Card
+                image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+                header='Elliot Baker'
+                //description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra={extra}
+            />
+             <Card
+                image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+                header='Elliot Baker'
+                //description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra={extra}
+            />
+             <Card
+                image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+                header='Elliot Baker'
+                //description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra={extra}
+            />
+             <Card
+                image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+                header='Elliot Baker'
+                //description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra={extra}
+            />
+             <Card
+                image='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+                header='Elliot Baker'
+                //description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+                extra={extra}
+            />
+        </Card.Group>
+        </Segment>
+    </div>
+</div>
 
 
